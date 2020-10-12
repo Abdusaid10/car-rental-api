f6bbdb6a7ff465ec22d1c32d666d64518c4abaa6
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-10-12 23:10:22 +0500

    change data types in the bookings table

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  # skip_before_action :authorize_request, only: %i[index create show]
+  skip_before_action :authorize_request, only: %i[index create]
   def index
     # @bookings = current_user.bookings
     @bookings = Booking.all
