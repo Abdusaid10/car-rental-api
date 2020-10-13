ad40a15ba53a1d9fce88a9fbe922538e7cea7753
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-10-13 16:03:05 +0500

    change origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://5f7c0f583e2aa9f3f205a57b--car-rental-client.netlify.app'
+    origins 'https://5f85892933cc66db0adb12ae--car-rental-client.netlify.app'
     # http://localhost:3001
-    # https://5f7c0f583e2aa9f3f205a57b--car-rental-client.netlify.app
+    # https://5f85892933cc66db0adb12ae--car-rental-client.netlify.app
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
